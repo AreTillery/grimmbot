6696b3ef88ead1b104fb52821809ecac6e9d6869
Tillery <63688713+AreTillery@users.noreply.github.com>
2020-04-20 00:16:09 +0000

    Add a command to allow for self-assigning certain roles

--- bot.py
+++ bot.py
@@ -9,9 +9,11 @@ import asyncio
 import time
 from datetime import datetime, timedelta
 
+moderatorbotchan = None
 botschan = None
 
-botschanid = "698583349433860106"
+moderatorbotschanid = "698583349433860106"
+botschanid = "701578731554078761"
 welcomeid = "699456430175944785"
 
 serverid = "696005227521900596"
@@ -39,10 +41,12 @@ async def on_member_join(member):
 async def on_ready():
     global server
     global botschan
+    global moderatorbotchan
     # Set up our objects
     if botschan is None:
         server = client.get_server(serverid)
         botschan = server.get_channel(botschanid)
+        moderatorbotchan = server.get_channel(moderatorbotschanid)
 #    await client.change_presence(game=discord.Game(name="sounds of Hacking", type=2))
     await client.change_presence(game=discord.Game(name="backdoors and breaches"))
     print("<hacker voice>I'm in</hacker voice>")
@@ -54,7 +58,7 @@ async def on_ready():
 
 # Command filter functions
 def is_botcommands_channel(ctx):
-    if ctx.message.channel.id == botschanid:
+    if ctx.message.channel.id in [botschanid, moderatorbotschanid]:
         return True
     role = discord.utils.find(lambda r: r.name.lower() == "grimm", ctx.message.server.roles)
     if role in ctx.message.author.roles:
@@ -130,7 +134,30 @@ async def mute (ctx, member:discord.User=None, reason=None):
     await client.add_roles(member, muterole)
     await client.send_message(ctx.message.channel, f"{member} is muted!")
 
-
+# publicly available commands. Have to be used from the public bot channel unless you have GRIMM role
+@client.command(pass_context=True)
+@commands.check(is_botcommands_channel)
+async def role(ctx, subcmd, newrole):
+    """ Add or remove a role. Valid roles: "blue-team", "red-team", "purple-team", "threat-hunting", "threat-modeling", "binary-analysis", "training" """
+    validroles = ["blue-team", "red-team", "purple-team", "threat-hunting", "threat-modeling", "binary-analysis", "training"]
+    if newrole not in validroles:
+        await client.send_message(ctx.message.channel, "USAGE: ~role <add|remove> <role>.\nInvalid role. Valid roles are: {}.".format(', '.join(validroles)))
+        return
+    roleobj = [role for role in server.roles if role.name == newrole][0]
+    if subcmd == "add":
+        if roleobj in ctx.message.author.roles:
+            await client.send_message(ctx.message.channel, f"You already have the {newrole} role!")
+            return
+        await client.add_roles(ctx.message.author, roleobj)
+        await client.send_message(ctx.message.channel, f"Done! You now have the {newrole} role!")
+    elif subcmd == "remove" or subcmd == "rem":
+        if roleobj not in ctx.message.author.roles:
+            await client.send_message(ctx.message.channel, f"You don't have the {newrole} role!")
+            return
+        await client.remove_roles(ctx.message.author, roleobj)
+        await client.send_message(ctx.message.channel, f"Done! Removed the {newrole} role!")
+    else:
+        await client.send_message(ctx.message.channel, "USAGE: ~role <add|remove> <role>.\nInvalid subcommand. Valid subcommands are: add, remove.")
 
 # END COMMAND HANDLERS
 
