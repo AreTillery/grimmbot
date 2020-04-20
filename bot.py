1a5f32fce70e50b8e651bb3a133bde8838223027
Tillery <tillery@tillery.fyi>
2020-04-20 01:59:36 +0000

    Add cyber-physical as an assignable role

--- bot.py
+++ bot.py
@@ -142,7 +142,7 @@ async def mute (ctx, member:discord.User=None, reason=None):
 @commands.check(is_botcommands_channel)
 async def role(ctx, subcmd, newrole):
     """ Add or remove a role. Valid roles: "blue-team", "red-team", "purple-team", "threat-hunting", "threat-modeling", "binary-analysis", "training" """
-    validroles = ["blue-team", "red-team", "purple-team", "threat-hunting", "threat-modeling", "binary-analysis", "training"]
+    validroles = ["blue-team", "red-team", "purple-team", "threat-hunting", "threat-modeling", "binary-analysis", "training", "cyber-physical"]
     if newrole not in validroles:
         await client.send_message(ctx.message.channel, "USAGE: ~role <add|remove> <role>.\nInvalid role. Valid roles are: {}.".format(', '.join(validroles)))
         return
